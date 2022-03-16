7cc2176927a515813027ff3540101348a807c896
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-15 21:04:52 -0700

    grabbed DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 // import functions and grab DOM elements
+const currentPollContainer = document.querySelector('#current-poll-container');
+const pollNameInput = document.querySelector('#poll-name-input');
+const optionOneInput = document.querySelector('#option-one-input');
+const optionTwoInput = document.querySelector('#option-two-input');
+const makePollButton = document.querySelector('#make-poll-button');
+const pollLabel = document.querySelector('#poll-name');
+const optionOneLabel = document.querySelector('#option-one-name');
+const optionTwoLabel = document.querySelector('#option-two-name');
+const optionOneAddButton = document.querySelector('#option-one-add-button');
+const optionOneSubtractButton = document.querySelector('#option-one-subtract-button');
+const optionTwoAddButton = document.querySelector('#option-two-add-button');
+const optionTwoSubtractButton = document.querySelector('#option-two-subtract-button');
+const resetPollButton = document.querySelector('#reset-poll-button');
 
-// let state
+let title = ;
+let name1 = ;
+let name2 = ;
+let score1 = ;
+let score2 = ;
 
 // set event listeners 
   // get user input
