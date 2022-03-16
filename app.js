89c5d30f4d99a81aed698a83b4214b5183494de3
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-03-15 21:25:29 -0700

    added all event listeners to app.js

--- app.js
+++ app.js
@@ -13,13 +13,63 @@ const optionTwoAddButton = document.querySelector('#option-two-add-button');
 const optionTwoSubtractButton = document.querySelector('#option-two-subtract-button');
 const resetPollButton = document.querySelector('#reset-poll-button');
 
-let title = ;
-let name1 = ;
-let name2 = ;
-let score1 = ;
-let score2 = ;
+let title = '';
+let name1 = '';
+let name2 = '';
+let score1 = 0;
+let score2 = 0;
 
 // set event listeners 
+makePollButton.addEventListener('click', () => {
+    title = pollNameInput.value;
+    name1 = optionOneInput.value;
+    name2 = optionTwoInput.value;
+
+    pollNameInput.value = '';
+    optionOneInput.value = '';
+    optionTwoInput.value = '';
+
+    // refresh the current game element with new data by calling the appropriate function
+    //refreshCurrentGameEl();
+});
+
+optionOneAddButton.addEventListener('click', () => {
+  // increment the current state for team one's score
+    score1++;
+
+  // refresh the current game element with new data by calling the appropriate function
+  //refreshCurrentGameEl();
+});
+
+optionTwoAddButton.addEventListener('click', () => {
+  // increment the current state for team one's score
+    score2++;
+
+  // refresh the current game element with new data by calling the appropriate function
+  //refreshCurrentGameEl();
+});
+
+optionOneSubtractButton.addEventListener('click', () => {
+  // increment the current state for team one's score
+    score1--;
+
+  // refresh the current game element with new data by calling the appropriate function
+  //refreshCurrentGameEl();
+});
+
+optionTwoSubtractButton.addEventListener('click', () => {
+  // increment the current state for team one's score
+    score2--;
+
+  // refresh the current game element with new data by calling the appropriate function
+  //refreshCurrentGameEl();
+});
+
+resetPollButton.addEventListener('click', () => {
+  
+})
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
